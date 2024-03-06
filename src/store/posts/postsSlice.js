3ffb2263e3c5174980cb886644bd106f9bed920e
garold664 <garold664@gmail.com>
2024-03-06 20:33:23 +0300

    changed the structure of the state in postsSlice

--- src/store/posts/postsSlice.js
+++ src/store/posts/postsSlice.js
@@ -2,40 +2,44 @@ import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'First Post!',
-    content:
-      'This is my first post. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor?',
-    userId: '0',
-    date: sub(new Date(), { hours: 3, minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 3,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0,
-      cat: 0,
+const initialState = {
+  posts: [
+    {
+      id: '1',
+      title: 'First Post!',
+      content:
+        'This is my first post. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor?',
+      userId: '0',
+      date: sub(new Date(), { hours: 3, minutes: 10 }).toISOString(),
+      reactions: {
+        thumbsUp: 3,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0,
+        cat: 0,
+      },
     },
-  },
-  {
-    id: '2',
-    title: 'Second Post!',
-    content:
-      'This is my second post. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor?',
-    userId: '0',
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 2,
-      cat: 0,
+    {
+      id: '2',
+      title: 'Second Post!',
+      content:
+        'This is my second post. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quas, maiores saepe culpa modi deleniti fugit! Ad autem ipsa sed molestiae eveniet optio ea corrupti, unde ducimus, aliquid eum dolor?',
+      userId: '0',
+      date: sub(new Date(), { minutes: 5 }).toISOString(),
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 2,
+        cat: 0,
+      },
     },
-  },
-];
+  ],
+  status: 'idle',
+  error: null,
+};
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -43,7 +47,7 @@ const postsSlice = createSlice({
   reducers: {
     addPost: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
 
       prepare(title, content, userId, reactions) {
@@ -62,7 +66,7 @@ const postsSlice = createSlice({
 
     updatePost: (state, action) => {
       const { id, title, content } = action.payload;
-      const existingPost = state.find((post) => post.id === id);
+      const existingPost = state.posts.find((post) => post.id === id);
 
       if (existingPost) {
         existingPost.title = title;
@@ -71,7 +75,7 @@ const postsSlice = createSlice({
     },
     addReaction(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id === postId);
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
@@ -83,7 +87,7 @@ export const { addPost, updatePost, addReaction } = postsSlice.actions;
 
 export default postsSlice.reducer;
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const selectPostById = (state, postId) =>
-  state.posts.find((post) => post.id === postId);
+  state.posts.posts.find((post) => post.id === postId);
